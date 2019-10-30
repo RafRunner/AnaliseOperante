39a47fe573650fa48e674f0bf200881b28e631f0
Rafael Nunes <rafaelns.br@gmail.com>
2019-10-29 22:23:25 -0300

    Primeiras anterações/configurações

---
+++ AnaliseOperante/source/services/AbstractService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaliseOperante.source.services {
+	abstract class AbstractService {
+
+		protected static string GetConnectionString(string id = "Default") {
+			return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+		}
+	}
+}
